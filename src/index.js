e736dc15a78978f3bdb395f07e062ea095b4d111
tajaneew <118587565+tajaneew@users.noreply.github.com>
2023-06-07 19:32:17 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+import ScotchInfoBar from "./ScotchInfoBar";
+
+/**
+ * Our data comes from users-data.js
+ * -----------------------------
+ */
+import users from "./users-data";
+
+/**
+ * Our React component where we display data
+ * -----------------------------
+ */
+function App() {
+  return (
+    <div className="App">
+      <div className="page-deets">
+        <h2>Loop Over an Array of Data</h2>
+      </div>
+
+      {/* Iterate over imported array in userData */}
+      <div className="users">
+        {/* Display each data in array in a card */}
+        {/* Each card must have a 'key' attribute */}
+
+        {users.map((user, index) => {
+          return (
+            <div key={index}>
+              <p>{user.name}</p>
+              <p>{user.location}</p>
+              <p>{user.car}</p>
+            </div>
+          );
+        })}
+      </div>
+      <ScotchInfoBar />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
